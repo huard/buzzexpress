102385194b22aa966d5c51c2cd72df0d2103497f
David Huard <huard.david@ouranos.ca>
2021-10-20 20:27:13 -0400

    bouton

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+    basic.clearScreen()
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "maitre") {
+        if (true) {
+            basic.showIcon(IconNames.SmallSquare)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    }
+})
+let groupe = 1
+radio.setGroup(groupe)
